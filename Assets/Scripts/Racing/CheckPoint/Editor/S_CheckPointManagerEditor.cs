2e4cd94746b981b80ba6ddabf89b53961773c69b
Takotzo <oyvindroberg@hotmail.no>
2025-09-17 11:36:55 +0200

    setting up custom editor

---
+++ Assets/Scripts/Racing/CheckPoint/Editor/S_CheckPointManagerEditor.cs
@@ -0,0 +1,10 @@
+using UnityEditor;
+using UnityEngine;
+
+public class S_CheckPointManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+    }
+}
--- Assets/Scripts/Racing/CheckPoint/S_CheckPointManager.cs
+++ Assets/Scripts/Racing/CheckPoint/S_CheckPointManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class S_CheckPointManager : MonoBehaviour
 {
@@ -68,6 +69,7 @@ public class S_CheckPointManager : MonoBehaviour
 
     #endregion
    
+    
     
     private void OnDrawGizmos()
     {
