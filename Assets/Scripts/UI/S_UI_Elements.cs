09f0bbf4942bc82d620f523d042e4fec36488f4c
BossmanCEO <hkalstad@gmail.com>
2025-09-25 11:41:36 +0200

    menumenu

--- Assets/Scripts/UI/S_UI_Elements.cs
+++ Assets/Scripts/UI/S_UI_Elements.cs
@@ -1,16 +1,90 @@
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.SceneManagement;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class S_UI_Elements : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private VisualTreeAsset mainMenuUXML;
+    [SerializeField] private VisualTreeAsset levelSelectUXML;
+    [SerializeField] private VisualTreeAsset optionsUXML;
+    [SerializeField] private VisualTreeAsset transferDataUXML;
+
+    
+    private UIDocument uiDocument;
+
+    private void Awake()
     {
         
+        uiDocument = GetComponent<UIDocument>();
+
+       
+        ShowMainMenu();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void RegisterCallbacks(VisualElement root)
     {
         
+
+        // Main Menu Buttons
+        if (root.Q<Button>("PlayBtn") != null)
+        {
+            root.Q<Button>("PlayBtn").clicked += ShowLevelSelect;
+            root.Q<Button>("OptionsBtn").clicked += ShowOptions;
+            root.Q<Button>("TransferDataBtn").clicked += ShowTransferData;
+            root.Q<Button>("QuitBtn").clicked += QuitGame;
+        }
+
+        // Level Select Buttons
+        if (root.Q<Button>("LevelSelectBackBtn") != null)
+        {
+            root.Q<Button>("LevelSelectBackBtn").clicked += ShowMainMenu;
+        }
+
+        var level1Btn = root.Q<Button>("Level1Btn");
+        if (level1Btn != null)
+        {
+            
+            level1Btn.clicked += () => LoadScene("Level1");
+        }
+
+        // Options Buttons
+        if (root.Q<Button>("OptionsBackBtn") != null)
+        {
+            root.Q<Button>("OptionsBackBtn").clicked += ShowMainMenu;
+        }
+
+        // Transfer Data Buttons
+        if (root.Q<Button>("TransferDataBackBtn") != null)
+        {
+            root.Q<Button>("TransferDataBackBtn").clicked += ShowMainMenu;
+        }
+    }
+
+    private void LoadAndShowMenu(VisualTreeAsset newUXML)
+    {       
+        uiDocument.visualTreeAsset = newUXML;
+
+        var root = uiDocument.rootVisualElement;
+
+        // Register button callbacks for the newly loaded menu.
+        RegisterCallbacks(root);
+    }
+
+    // Public methods to be called by buttons
+    private void ShowMainMenu() => LoadAndShowMenu(mainMenuUXML);
+    private void ShowLevelSelect() => LoadAndShowMenu(levelSelectUXML);
+    private void ShowOptions() => LoadAndShowMenu(optionsUXML);
+    private void ShowTransferData() => LoadAndShowMenu(transferDataUXML);
+
+    private void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+
+    private void LoadScene(string sceneName)
+    {
+        Debug.Log($"Loading scene: {sceneName}");
+        SceneManager.LoadScene(sceneName);
     }
 }
