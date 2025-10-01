06cd55769bda609adee5242b15414e1c0e5dcdce
anatolmo <145755035+anatolmo@users.noreply.github.com>
2025-10-01 14:35:18 +0200

    Update

--- Assets/Scripts/Math/S_PauseManager.cs
+++ Assets/Scripts/Math/S_PauseManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class S_PauseManager : MonoBehaviour
 {
+    [SerializeField] private GameObject MathManager;
+    [SerializeField] private GameObject VisualManager;
     private bool isPaused = false;
 
     public void TogglePause()
@@ -16,11 +18,21 @@ public class S_PauseManager : MonoBehaviour
     {
         Time.timeScale = 0f;
         isPaused = true;
+
+        if (VisualManager != null)
+            VisualManager.SetActive(false);
+        if (MathManager != null)
+            MathManager.SetActive(false);
     }
 
     public void ResumeGame()
     {
         Time.timeScale = 1f;
         isPaused = false;
+
+        if (VisualManager != null)
+            VisualManager.SetActive(true);
+        if (MathManager != null)
+            MathManager.SetActive(true);
     }
 }
